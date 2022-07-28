5a8a0fdba511373389e345187030599a757c4803
abrilramosa <abrilramosa@gmail.com>
2022-07-28 18:51:43 +0200

    Ejercicio 9. Javascript

---
+++ ejercicio9.js
@@ -0,0 +1,15 @@
+// Part1. Get random number
+
+function getRandom(){
+    return Math.round(Math.random());
+}
+
+console.log(getRandom());
+
+// Part2. Get round number
+
+function getFloor(x) {
+    return Math.floor(x);
+ }
+
+console.log(getFloor(3,1416));
