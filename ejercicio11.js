811cc24aee8870936626bb0206595de4c727096c
abrilramosa <abrilramosa@gmail.com>
2022-07-29 19:32:44 +0200

    Ejercicio11.Javascript

---
+++ ejercicio11.js
@@ -0,0 +1,14 @@
+// Bucle for 10 veces I love code
+function repeatTenTimes (frase){
+for (let index = 0; index < 10; index++) {
+    console.log(frase);
+}}
+repeatTenTimes("I ❤️ code");
+
+// Bucle while 10 veces I love code
+frase = "I ❤️ code";
+let i = 1;
+while (i < 10) {
+    console.log(frase);
+    i++;
+}
